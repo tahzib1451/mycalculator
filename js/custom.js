d588a648b9525f819e8f1cf9fa64500044ea8e16
Tahzib Hussaini <thussaini121004@bseco.uiu.ac.bd>
2022-04-05 09:48:27 +0600

    final commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+const outputScreen = document.getElementById('output');
+function display(num) {
+    outputScreen.value += num;
+};
+function Calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch (error) {
+        alert("Not Defined");
+    }
+};
+function Clear() {
+    outputScreen.value = "";
+};
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+};
